d4f220a132e4e70ccf1dfbc4fb9ed7b63b9395f5
erkan7536 <103214905+erkan7536@users.noreply.github.com>
2023-04-26 02:06:38 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ function enterKey(e) {
     if (e.keyCode == 13) {
       commands.push(command.innerHTML);
       git = commands.length;
-      addLine("root@recoo:~$ " + command.innerHTML, "no-animation", 0);
+      addLine("root@jofir:~$ " + command.innerHTML, "no-animation", 0);
       commander(command.innerHTML.toLowerCase());
       command.innerHTML = "";
       textarea.value = "";
